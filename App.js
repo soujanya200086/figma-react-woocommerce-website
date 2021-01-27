1bc61a4583ce520efbed0f90debea39c165380d1
soujanya200086 <78073602+soujanya200086@users.noreply.github.com>
2021-01-27 13:48:39 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+
+import Hero from "./components/HeroText"
+
+
+
+
+
+const App = () => {
+  return (
+ <>
+ <Navbar/>
+ <Hero/>
+ 
+
+    </> 
+   
+  );
+};
+
+
+
+export default App;
